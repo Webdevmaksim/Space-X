e26879bccb5691026b06dc75ae1be7b0a5f543ed
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-08-05 11:46:08 +0300

    FEAT: animated rocket

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+//Video Bg
 var instance = new vidbg('.video', {
     mp4: '../video/world.mp4', // URL or relative path to MP4 video
     webm: '../video/world.webm', // URL or relative path to webm video
     poster: '../video/poster.jpg', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   });
+//paralax
+  var rellax = new Rellax('.rocket');
